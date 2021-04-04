55edd43f04e2c83fd904413250e1f89c9ad331ff
dighumcld <91414507+dighumcld@users.noreply.github.com>
2021-04-04 06:16:30 +0000

    Updated the conda-smithy feedstock.

--- build.sh
+++ build.sh
@@ -3,3 +3,24 @@
 
 function nuke() {
   rm -rf bin/* && rm -rf lib/* && rm -rf build/*
+}
+
+function uninstall() {
+  xargs rm < build/install_manifest.txt
+}
+
+function build() {
+  mkdir -p build
+  cd build
+  cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=.. -DBUILD_SHARED_LIBS=ON
+  make && make install
+}
+
+if [[ $1 == "nuke" ]]; then
+  nuke
+elif [[ $1 == "uninstall" ]]; then
+  uninstall
+else
+  uninstall
+  build
+fi
