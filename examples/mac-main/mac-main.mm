94a59afc47c1186d6b62d4eafe83c2d1327267a7
dighumcld <91414507+dighumcld@users.noreply.github.com>
2021-04-18 09:51:18 +0000

    bump to v0.1.1

---
+++ examples/mac-main/mac-main.mm
@@ -0,0 +1,10 @@
+
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include "DeepCapture.h"
+#include "MacDisplayCapture.h"
+
+#import <CoreGraphics/CGDisplayStream.h>
+#import <Cocoa/Cocoa.h>
