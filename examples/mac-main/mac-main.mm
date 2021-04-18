4d14d9a2f87cb0981daffe5e144c5fc965780d7c
dighumcld <91414507+dighumcld@users.noreply.github.com>
2021-04-18 10:15:08 +0000

    working on context

--- examples/mac-main/mac-main.mm
+++ examples/mac-main/mac-main.mm
@@ -8,3 +8,24 @@
 
 #import <CoreGraphics/CGDisplayStream.h>
 #import <Cocoa/Cocoa.h>
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+#define MAX_DISPLAYS 32
+
+CGDisplayStreamRef stream_ref;
+int display_index = 0;
+
+clock_t last_tick = 0;
+clock_t fps = 0;
+int fpassed = 0;
+
+int width = 0;
+int height = 0;
+
+pthread_mutex_t mutex_update;
+
+void update(CGDisplayStreamFrameStatus status,
+      uint64_t displayTime,
