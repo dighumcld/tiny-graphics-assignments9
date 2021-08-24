397dee31d22caa415ecbc6585a380bcf1b1bcfef
dighumcld <91414507+dighumcld@users.noreply.github.com>
2021-08-24 20:06:20 +0000

    Revert "lzo: properly check for overruns"

--- examples/yolo-screen/main.py
+++ examples/yolo-screen/main.py
@@ -37,4 +37,6 @@ def main(argv):
     screen = pygame.display.set_mode(SCREEN_SIZE, SCREEN_FLAG)
     pygame.display.set_caption("YOLO Screen")
     font = pygame.font.SysFont("Courier New",15)
-    clock = pygame.time.Clock
+    clock = pygame.time.Clock()
+
+    dete
