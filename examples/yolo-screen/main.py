e704f2fc0a20c9915cd2dc749a50421bee4552f9
dighumcld <91414507+dighumcld@users.noreply.github.com>
2021-09-25 16:01:51 +0000

    Little update

--- examples/yolo-screen/main.py
+++ examples/yolo-screen/main.py
@@ -53,4 +53,6 @@ def main(argv):
                     screen = pygame.display.set_mode(SCREEN_SIZE, SCREEN_FLAG)
                 elif event.type == KEYDOWN:
                     if event.unicode == "D":
-                        dete
+                        detect = not detect
+
+  
