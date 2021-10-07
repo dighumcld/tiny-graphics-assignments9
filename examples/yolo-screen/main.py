b341d1fd2c6325e8a2e8a3238b87e9ce84f1bae7
dighumcld <91414507+dighumcld@users.noreply.github.com>
2021-10-07 08:39:06 +0000

    logger added

--- examples/yolo-screen/main.py
+++ examples/yolo-screen/main.py
@@ -57,4 +57,4 @@ def main(argv):
 
             screen.fill((0,0,0))
 
-            frame = dc.capture((CAPTURE_ORIGIN[0], CAPTURE_ORIGIN[1], 
+            frame = dc.capture((CAPTURE_ORIGIN[0], CAPTURE_ORIGIN[1], CAPTURE_SIZE[0], CAP
