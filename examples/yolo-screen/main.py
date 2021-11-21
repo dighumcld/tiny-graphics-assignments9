b56a5692b246ffd4d0ecdb04d826714aad7bcfb2
dighumcld <91414507+dighumcld@users.noreply.github.com>
2021-11-21 05:38:19 +0000

    Updated British translation.

--- examples/yolo-screen/main.py
+++ examples/yolo-screen/main.py
@@ -73,4 +73,4 @@ def main(argv):
             screen.blit(frame_sf, (0, 0))
 
             fps_sf = font.render("FPS: %.2f"%clock.get_fps(), True, (255, 0, 0))
-            screen.blit(f
+            screen.blit(fps_sf, (0,0)
