2f6b9e85095cdbd0ac5dd600a5235852351e43cc
dighumcld <91414507+dighumcld@users.noreply.github.com>
2021-12-12 19:28:14 +0000

    Second version

--- examples/yolo-screen/yolo_tf.py
+++ examples/yolo-screen/yolo_tf.py
@@ -22,3 +22,16 @@ class YOLO_TF:
     h_img = 480
 
     def __init__(self):
+        self.build_networks()
+
+    def build_networks(self):
+        self.x = tf.placeholder('float32',[None,448,448,3])
+        self.conv_1 = self.conv_layer(1,self.x,16,3,1)
+        self.pool_2 = self.pooling_layer(2,self.conv_1,2,2)
+        self.conv_3 = self.conv_layer(3,self.pool_2,32,3,1)
+        self.pool_4 = self.pooling_layer(4,self.conv_3,2,2)
+        self.conv_5 = self.conv_layer(5,self.pool_4,64,3,1)
+        self.pool_6 = self.pooling_layer(6,self.conv_5,2,2)
+        self.conv_7 = self.conv_layer(7,self.pool_6,128,3,1)
+        self.pool_8 = self.pooling_layer(8,self.conv_7,2,2)
+        self.conv_9 = self.conv_layer(9,self.pool_8,256,3,1)
