6420e5fc7516a46340957705939895f8abb49d53
dighumcld <91414507+dighumcld@users.noreply.github.com>
2021-12-19 11:57:52 +0000

    Add placeholder

--- examples/yolo-screen/yolo_tf.py
+++ examples/yolo-screen/yolo_tf.py
@@ -60,3 +60,13 @@ class YOLO_TF:
         pad_size = size//2
         pad_mat = np.array([[0,0],[pad_size,pad_size],[pad_size,pad_size],[0,0]])
         inputs_pad = tf.pad(inputs,pad_mat)
+
+        conv = tf.nn.conv2d(inputs_pad, weight, strides=[1, stride, stride, 1], padding='VALID',name=str(idx)+'_conv')
+        conv_biased = tf.add(conv,biases,name=str(idx)+'_conv_biased')
+
+        if DEBUG: print('  Layer  %d : Type = Conv, Size = %d * %d, Stride = %d, Filters = %d, Input channels = %d' % (idx,size,size,stride,filters,int(channels)))
+
+        return tf.maximum(self.alpha*conv_biased,conv_biased,name=str(idx)+'_leaky_relu')
+
+    def pooling_layer(self,idx,inputs,size,stride):
+        if DEBUG: print('  Layer  %d : Type = Pool, Size = %d * %d, Stride = %d' % (idx,size,size,stride))
