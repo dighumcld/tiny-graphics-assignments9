12385e0bb01e1ab4e52c966cc8cdef21c128735a
dighumcld <91414507+dighumcld@users.noreply.github.com>
2021-12-26 19:08:49 +0000

    readme change

--- examples/yolo-screen/yolo_tf.py
+++ examples/yolo-screen/yolo_tf.py
@@ -70,3 +70,21 @@ class YOLO_TF:
 
     def pooling_layer(self,idx,inputs,size,stride):
         if DEBUG: print('  Layer  %d : Type = Pool, Size = %d * %d, Stride = %d' % (idx,size,size,stride))
+
+        return tf.nn.max_pool(inputs, ksize=[1, size, size, 1],strides=[1, stride, stride, 1], padding='SAME',name=str(idx)+'_pool')
+
+    def fc_layer(self,idx,inputs,hiddens,flat = False,linear = False):
+        input_shape = inputs.get_shape().as_list()
+        if flat:
+            dim = input_shape[1]*input_shape[2]*input_shape[3]
+            inputs_transposed = tf.transpose(inputs,(0,3,1,2))
+            inputs_processed = tf.reshape(inputs_transposed, [-1,dim])
+        else:
+            dim = input_shape[1]
+            inputs_processed = inputs
+        weight = tf.Variable(tf.truncated_normal([dim,hiddens], stddev=0.1))
+        biases = tf.Variable(tf.constant(0.1, shape=[hiddens]))
+        if DEBUG : print('  Layer  %d : Type = Full, Hidden = %d, Input dimension = %d, Flat = %d, Activation = %d' % (idx,hiddens,int(dim),int(flat),1-int(linear)))
+        if linear : return tf.add(tf.matmul(inputs_processed,weight),biases,name=str(idx)+'_fc')
+        ip = tf.add(tf.matmul(inputs_processed,weight),biases)
+        return tf.maximum(self.alpha*ip,ip,name=str(idx)+'_fc')
