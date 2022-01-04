ae830486e468ad5eb41cada5f89a2625cdc4378b
dighumcld <91414507+dighumcld@users.noreply.github.com>
2022-01-04 13:28:41 +0000

    chore: regenerate README

--- examples/yolo-screen/yolo_tf.py
+++ examples/yolo-screen/yolo_tf.py
@@ -128,3 +128,17 @@ class YOLO_TF:
         boxes[:,:,:,1] *= self.h_img
         boxes[:,:,:,2] *= self.w_img
         boxes[:,:,:,3] *= self.h_img
+
+        for i in range(2):
+            for j in range(20):
+                probs[:,:,i,j] = np.multiply(class_probs[:,:,j],scales[:,:,i])
+
+        filter_mat_probs = np.array(probs>=self.threshold,dtype='bool')
+        filter_mat_boxes = np.nonzero(filter_mat_probs)
+        boxes_filtered = boxes[filter_mat_boxes[0],filter_mat_boxes[1],filter_mat_boxes[2]]
+        probs_filtered = probs[filter_mat_probs]
+        classes_num_filtered = np.argmax(filter_mat_probs,axis=3)[filter_mat_boxes[0],filter_mat_boxes[1],filter_mat_boxes[2]]
+
+        argsort = np.array(np.argsort(probs_filtered))[::-1]
+        boxes_filtered = boxes_filtered[argsort]
+        probs_filtered = probs_filtered[argsort]
