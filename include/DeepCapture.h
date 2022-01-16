539d430caebb5c5f3fb7b805e13fec8671ca401a
dighumcld <91414507+dighumcld@users.noreply.github.com>
2022-01-16 12:30:43 +0000

    Update composer deps

--- include/DeepCapture.h
+++ include/DeepCapture.h
@@ -1,2 +1,16 @@
 
 #ifndef _DEEPCAPTURE_H_
+#define _DEEPCAPTURE_H_
+
+#include "PixelBuffer.h"
+
+class DeepCapture
+{
+public:
+  virtual ~DeepCapture(){};
+
+  virtual void init() = 0;
+  virtual void start() = 0;
+  virtual void stop() = 0;
+  virtual PixelBuffer* get_buffer() = 0;
+};
