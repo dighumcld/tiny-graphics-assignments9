18107435b5072c5ab5616d541100e01d03a77181
dighumcld <91414507+dighumcld@users.noreply.github.com>
2022-05-06 19:06:48 +0000

    Upgraded gem(s): pg

--- include/PixelBuffer.h
+++ include/PixelBuffer.h
@@ -32,4 +32,5 @@ public:
   	assert(y + h <= height);
 
   	PixelBuffer* buf = new PixelBuffer(w, h, bpp);
-  	uint8_t* ps = buffer + x * bpp + y * width * bpp
+  	uint8_t* ps = buffer + x * bpp + y * width * bpp;
+  	uint8_t* p
