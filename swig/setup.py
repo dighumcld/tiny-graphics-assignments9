71491276327a1308192885eefd3d9d49d1c16308
dighumcld <91414507+dighumcld@users.noreply.github.com>
2023-04-02 13:14:09 +0000

    f2fs: Fix a system panic caused by f2fs_follow_link

--- swig/setup.py
+++ swig/setup.py
@@ -9,3 +9,17 @@ setup(
     name='deep-capture',
     version='0.0.1',
     author='Zihao Zhang',
+    author_email="zzh8829@gmail.com",
+    description='Video Capture for Deep Learning',
+    long_description='N/A',
+    url='https://github.com/zzh8829/deep-capture',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics :: Capture :: Screen Capture"
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+    ]
+)
